4c6fd4b01985db901abaec167759156734ed9947
Andy Maskiell <maskiella@gmail.com>
2022-07-17 12:18:38 -0400

    More project setup :) (aka installing more packages)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,15 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `wait, wut?`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'wait, wut?',
+    siteUrl: 'https://www.yourdomain.tld',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: '/waitwut',
   plugins: [],
 }
 
