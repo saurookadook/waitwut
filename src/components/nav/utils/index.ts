0f25fc7554a3cd97c5130384cd7356434813a52f
Andy Maskiell <maskiella@gmail.com>
2022-11-06 12:54:52 -0500

    Add childSections to Bookmarks in pageMap, create utils for nav components

---
+++ src/components/nav/utils/index.ts
@@ -0,0 +1,73 @@
+import { PageMap } from "../../../constants/pageMap";
+
+interface AddNodesToChildrenArgs {
+    nodes: NodeFromQuery[];
+    children: NavLinkItem[];
+}
+
+function addNodesToChildren({
+    nodes,
+    children
+}: AddNodesToChildrenArgs): void {
+    nodes.forEach((node: NodeFromQuery) => {
+        children && children.push({
+            slug: node.slug.replace(/(\w+?\/){1,}/g, ""),
+            label: node.frontmatter?.title || node.slug,
+            iconName: node.frontmatter?.iconComponentName
+            // path: node.frontmatter?.fullPath
+        });
+    });
+}
+
+interface CreateNavLinksArgs {
+    nodesGroups: GroupFromQuery[];
+    pageMap: PageMap[];
+    parentNavLink?: NavLinkItem;
+    navLinksResult?: NavLinkItem[];
+}
+
+function createNavLinks({
+    nodesGroups,
+    pageMap,
+    parentNavLink,
+    navLinksResult = []
+}: CreateNavLinksArgs): NavLinkItem[] {
+
+    for (const page of pageMap) {
+        const navLink = {
+            slug: page.sectionSlug,
+            label: page.title,
+            children: [] as NavLinkItem[]
+        };
+
+        const nodeGroup = nodesGroups.find((node) => node.fieldValue === page.sectionSlug);
+
+        if (nodeGroup && nodeGroup.nodes) {
+            addNodesToChildren({
+                nodes: nodeGroup?.nodes,
+                children: navLink.children
+            });
+        }
+
+        if (parentNavLink && parentNavLink.children) {
+            parentNavLink.children.push(navLink);
+        } else {
+            navLinksResult.push(navLink);
+        }
+
+        if (page.childSections) {
+            navLinksResult = createNavLinks({
+                nodesGroups,
+                pageMap: page.childSections,
+                parentNavLink: navLink,
+                navLinksResult
+            });
+        }
+    }
+
+    return navLinksResult;
+}
+
+export {
+    createNavLinks
+}
--- src/constants/pageMap.ts
+++ src/constants/pageMap.ts
@@ -1,5 +1,14 @@
+export interface PageMap {
+    color?: string;
+    description?: string;
+    sectionSlug: string;
+    title: string;
+    topParentSlug?: string;
+    childSections?: PageMap[];
+}
+
 // TODO: this seems like a hack for basically just querying the top-level of `pages`...
-const pageMap: Record<string, string>[] = [ // TODO: better typing :)
+const pageMap: PageMap[] = [ // TODO: better typing :)
     {
         color: '#E95800',
         description: 'Mix of notes and cheat sheets 🙂',
@@ -11,6 +20,15 @@ const pageMap: Record<string, string>[] = [ // TODO: better typing :)
         description: 'My Library-of-Alexandria-sized collection of bookmarks. 🤓',
         sectionSlug: 'bookmarks',
         title: 'Bookmarks',
+        childSections: [
+            {
+                color: '#E95800',
+                description: 'TODO 🙃',
+                sectionSlug: 'code',
+                title: 'Code',
+                topParentSlug: 'bookmarks'
+            }
+        ]
     },
 ];
 
