d86d21eee4d6071e27d48d80c74ca75fb7fa2fa5
Andy Maskiell <andy-maskiell@pluralsight.com>
2023-06-01 16:22:58 -0400

    Move toTitleCase to utils

---
+++ src/utils/index.tsx
@@ -0,0 +1,6 @@
+export const toTitleCase = (string: string): string => {
+    return string.replace(/[A-Z]\w*?(?=[A-Z]|$)/gm, (match, ...args) => {
+        const offsetIndex = typeof args.at(-1) === "object" ? args.at(-3) : args.at(-2);
+        return offsetIndex === 0 ? `${match}` : ` ${match}`;
+    });
+};
