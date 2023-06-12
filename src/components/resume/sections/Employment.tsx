32207f03bf3befc3eec2d765e799efc9d8c158ec
Andy Maskiell <andy-maskiell@pluralsight.com>
2023-06-12 09:27:56 -0400

    Several styling improvements to Employment component

--- src/components/resume/sections/Employment.tsx
+++ src/components/resume/sections/Employment.tsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import styled from 'styled-components';
+import { themeColors, resumeTheme } from '../../../themes';
 
 const EmploymentItemContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: auto;
     z-index: 1;
 
     &:hover {
         cursor: pointer;
+    }
+
+    &.collapsed {
+        flex: 0;
+        max-height: auto;
+        transition: all 300ms ease-out;
+    }
+
+    &.expanded {
+        flex: 1;
+        max-height: 100%;
+        transition: all 300ms ease-in;
     }
 `;
 
@@ -18,11 +34,16 @@ const CompanyName = styled.h3`
 `;
 
 const ExpandableDetails = styled.div`
+    /* &,
+    & *:not(span) { */
     display: flex;
     flex-direction: column;
+    margin: 0;
     z-index: 0;
+    /* } */
 
-    &.collapsed {
+    &.hidden,
+    &.hidden * {
         flex: 0;
         height: 0;
         max-height: 0;
@@ -30,7 +51,8 @@ const ExpandableDetails = styled.div`
         transition: all 300ms ease-out;
     }
 
-    &.expanded {
+    &.visible,
+    &.visible * {
         flex: 1;
         height: auto;
         max-height: 100%;
@@ -46,32 +68,55 @@ interface EmploymentItemProps {
 const EmploymentItem = ({ employmentRecord }: EmploymentItemProps): React.ReactElement => {
     const [isCollapsed, setIsCollapsed] = useState(true);
 
-    const handleToggleOnClick = (event: MouseEvent): void => {
-        event.stopPropagation();
-        setIsCollapsed(!isCollapsed);
-    };
+    const handleToggleOnClick = (): void => setIsCollapsed(!isCollapsed);
+
+    const { company, roles, responsibilities } = employmentRecord;
 
     return (
-        <EmploymentItemContainer>
-            <CompanyName onClick={handleToggleOnClick}>{employmentRecord.company.name}</CompanyName>
-            <ExpandableDetails className={isCollapsed ? 'collapsed' : 'expanded'}>
-                {typeof employmentRecord === 'string' ? (
-                    employmentRecord
-                ) : (
-                    <pre>{JSON.stringify(employmentRecord, null, 4)}</pre>
-                )}
+        <EmploymentItemContainer
+            onClick={handleToggleOnClick as MouseEventHandler}
+            className={isCollapsed ? 'collapsed' : 'expanded'}
+        >
+            <CompanyName>{company.name}</CompanyName>
+            <ExpandableDetails className={isCollapsed ? 'hidden' : 'visible'}>
+                <div>
+                    <span>
+                        <b>Location:</b> {company.location.city}, {company.location.state}
+                    </span>
+                    <span>
+                        {roles.map(
+                            (role, i): React.ReactElement => (
+                                <span key={`role-item-${i}`}>
+                                    <b>{role.title}</b> :: <em>{`${role.startDate} - ${role.endDate}`}</em>
+                                </span>
+                            ),
+                        )}
+                    </span>
+                    {responsibilities == null ? null : (
+                        <span>
+                            <ul>
+                                {responsibilities.map((responsibility, j) => (
+                                    <li key={`responsibility-${j}`}>{responsibility}</li>
+                                ))}
+                            </ul>
+                        </span>
+                    )}
+                </div>
             </ExpandableDetails>
         </EmploymentItemContainer>
     );
 };
 
-const EmploymentContainer = styled.div``;
+const EmploymentContainer = styled.div`
+    background-color: ${themeColors.darkerPurpleHex};
+    color: ${themeColors.white};
+    padding: 1em 10vw;
+`;
 
 const Employment = ({ heading, data }: SectionComponentProps): React.ReactElement => {
     return (
         <EmploymentContainer>
             <h2>{heading}</h2>
-            <span>Under Construction 🚧</span>
             {data.map((record, i) => (
                 <EmploymentItem key={`${record.company.name}-${i}`} employmentRecord={record} />
             ))}
