7f7310077d08aa0d9edcbaad4e5fff279fad0ffe
Andy Maskiell <andy-maskiell@pluralsight.com>
2023-06-21 10:19:04 -0400

    Install Pando and add styles and fonts to gatsby-ssr

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -6,7 +6,27 @@ require("prismjs/themes/prism-tomorrow.css"); // closest one to Atom One Dark, I
 const React = require('react'); // const causes a conflict?
 const { Layout } = require('./src/components');
 
-exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
+const HeadComponents = [
+    <meta name="viewport" content="initial-scale=1, width=device-width" />,
+    <link
+        rel="preload"
+        href="https://fonts.pluralsight.com/ps-tt-commons/v1/ps-tt-commons-variable-roman.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="true"
+    />,
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@pluralsight/design-tokens@0.4.1/fonts.css"
+    />,
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@pluralsight/design-tokens@0.4.1/npm/normalize/normalize.css"
+    />
+]
+
+exports.onRenderBody = ({ setHeadComponents, setBodyAttributes }, pluginOptions) => {
+    setHeadComponents(HeadComponents)
     setBodyAttributes({
         id: "waitwut-body",
     });
