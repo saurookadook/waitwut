97c7b594a3ada205932d4369567ab4238bbf1edc
Andy Maskiell <maskiella@gmail.com>
2023-08-14 13:00:29 -0400

    Leave TODO in utils

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,10 +1,24 @@
 export const toTitleCase = (string: string): string => {
     return string.replace(/[A-Z]\w*?(?=[A-Z]|$)/gm, (match, ...args) => {
-        const offsetIndex = typeof args.at(-1) === "object" ? args.at(-3) : args.at(-2);
+        const offsetIndex = typeof args.at(-1) === 'object' ? args.at(-3) : args.at(-2);
         return offsetIndex === 0 ? `${match}` : ` ${match}`;
     });
 };
 
 export const toKebabCase = (string: string): string => {
     return string.trim().replace(/\s+/gim, '-');
-}
+};
+
+// TODO: I feel like this would be nicer :)
+// // @ts-ignore
+// String.prototype.__proto__.toTitleCase = (string: string): string => {
+//     return string.replace(/[A-Z]\w*?(?=[A-Z]|$)/gm, (match, ...args) => {
+//         const offsetIndex = typeof args.at(-1) === 'object' ? args.at(-3) : args.at(-2);
+//         return offsetIndex === 0 ? `${match}` : ` ${match}`;
+//     });
+// };
+
+// // @ts-ignore
+// String.prototype.__proto__.toKebabCase = (string: string): string => {
+//     return string.trim().replace(/\s+/gim, '-');
+// };
