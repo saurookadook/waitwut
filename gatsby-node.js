1f3d81ea1bcaf9d3027505aea0f661e8b9e9aec5
Andy Maskiell <maskiella@gmail.com>
2023-08-19 23:27:20 -0400

    Add gatsby-node with resolve aliases

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                common: path.resolve(__dirname, 'src/common/'),
+                components: path.resolve(__dirname, 'src/components/'),
+                constants: path.resolve(__dirname, 'src/constants/'),
+                pages: path.resolve(__dirname, 'src/pages/'),
+                resume: path.resolve(__dirname, 'src/components/resume/'),
+                themes: path.resolve(__dirname, 'src/themes/'),
+                utils: path.resolve(__dirname, 'src/utils/'),
+            },
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        },
+    });
+};
