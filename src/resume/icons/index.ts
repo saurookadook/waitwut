3dbf73ff4d2dc15380e17292d8d4d1de18324208
Andy Maskiell <maskiella@gmail.com>
2023-08-26 16:22:07 -0400

    Add github icon

--- src/resume/icons/index.ts
+++ src/resume/icons/index.ts
@@ -1,6 +1,7 @@
 import BSOCombinedLogo from './bso';
 // import EvergageIcon from './evergage';
 import EvergageLogoVector from './evergage-logo';
+import GitHubOctocat from './github';
 import PluralsightLogo from './pluralsight';
 import SalesforceIcon from './salesforce';
 import UpstatementU from './upstatement';
@@ -10,6 +11,7 @@ const resumeIconsMap = {
     "boston-symphony-orchestra": BSOCombinedLogo,
     "evergage": EvergageLogoVector,
     // "evergage": EvergageIcon,
+    "github": GitHubOctocat,
     "pluralsight": PluralsightLogo,
     "salesforce": SalesforceIcon,
     "upstatement": UpstatementU,
