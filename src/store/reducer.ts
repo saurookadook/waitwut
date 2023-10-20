d7b95b8f774e350f931818b6b81e7e24aeb0dacd
Andy Maskiell <andy-maskiell@pluralsight.com>
2023-10-20 14:52:12 -0400

    Add new actionType, add CLOSE_MENU to actions and reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,15 @@
+import { actionTypes } from 'common/constants';
 import { combineReducers } from './utils';
 
 const menu: GenericStateSliceReducer<MenuStateSlice, BaseReducerAction> = [
     (stateSlice, action) => {
         switch (action.type) {
-            case 'TOGGLE_MENU_VISIBILITY':
+            case actionTypes.CLOSE_MENU:
+                return {
+                    ...stateSlice,
+                    drawerVisible: false,
+                };
+            case actionTypes.TOGGLE_MENU_VISIBILITY:
                 return {
                     ...stateSlice,
                     drawerVisible: !stateSlice.drawerVisible,
