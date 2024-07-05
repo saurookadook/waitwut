1f9a9cd3db6b89bf40fedd9ac12aab21595cc356
Andy Maskiell <maskiella@gmail.com>
2024-07-04 22:17:41 -0400

    Update gatsby-config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,16 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-    siteMetadata: {
-        title: 'wait, wut?',
-        siteUrl: 'https://saurookadook.github.io',
-    },
     // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
     // If you use VSCode you can also use the GraphQL plugin
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
-    pathPrefix: '/waitwut',
+    siteMetadata: {
+        title: 'wait, wut?',
+        // siteUrl: 'https://saurookadook.github.io',
+        siteUrl: 'https://waitwut.xyz',
+    },
+    // pathPrefix: '/waitwut',
     plugins: [
         `gatsby-plugin-netlify`,
         'gatsby-plugin-tsconfig-paths',
