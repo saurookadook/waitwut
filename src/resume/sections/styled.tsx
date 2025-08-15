5f88e38241792ec4759e526146530c0c660df6c8
Andy Maskiell <maskiella@gmail.com>
2025-08-15 14:57:29 -0400

    feat: minor update to prettierrc, minor cleanup

--- src/resume/sections/styled.tsx
+++ src/resume/sections/styled.tsx
@@ -1,13 +1,31 @@
 const collapsedStyles = `
     max-height: 0;
     opacity: 0;
-    transition: all 500ms ease-in;
+    /* transition:
+      background-color 500ms,
+      color 500ms,
+      flex 500ms,
+      height 500ms,
+      max-height 500ms 200ms,
+      opacity 250ms; */
+
+    transition: all 500ms;
+    transition-timing-function: ease-in;
 `;
 
 const expandedStyles = `
     max-height: 100%;
     opacity: 1;
-    transition: all 300ms ease-in;
+    /* transition:
+      background-color 300ms,
+      color 300ms,
+      flex 300ms,
+      height 300ms,
+      max-height 300ms,
+      opacity 300ms; */
+
+    transition: all 300ms;
+    transition-timing-function: ease-in;
 `;
 
 export { collapsedStyles, expandedStyles };
